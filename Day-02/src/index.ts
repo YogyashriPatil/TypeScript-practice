e5eda3c5080ef73ecd8992d1149044da9b4af95b
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-14 20:25:30 +0530

    starting the day 2

---
+++ Day-02/src/index.ts
@@ -0,0 +1,11 @@
+let name = "yogyashri"
+let cups = Math.random()
+
+console.log(name)
+console.log(cups)
+
+let firstname:string="yogyashri"
+let age:number=12
+
+console.log(firstname)
+console.log(age)
