09d2f81cedb07f3814331bc284287149b6a9a103
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-14 20:31:30 +0530

    Learn about the union

---
+++ Day-02/src/UnionAndany.ts
@@ -0,0 +1,14 @@
+let subs: number | string = '1M'
+
+let apiRequest:'pending'|'success'|'error' = "pending"
+// apiRequest=''
+console.log(apiRequest)
+
+let airlines: "aisle"|'window' | 'middle' = "aisle"
+
+airlines = "window"
+
+console.log(airlines)
+
+
+let currentOrders: string|undefined
